4e7cad42cb82b63ede6d8efd711e07b3c6bed469
Muriel <matheus.muriel@outlook.com>
2019-10-14 15:17:18 -0300

    MiniMaxSum comcluido

--- Mini-MaxSum/Mini-MaxSum.rb
+++ Mini-MaxSum/Mini-MaxSum.rb
@@ -7,8 +7,8 @@ require 'stringio'
 
 # Complete the miniMaxSum function below.
 def miniMaxSum(arr)
-
-
+    resp = arr.combination(4).minmax_by{|x| x.sum}.map{|x| x.sum}
+    puts resp[0].to_s + " " + resp[1].to_s
 end
 
 arr = gets.rstrip.split(' ').map(&:to_i)
