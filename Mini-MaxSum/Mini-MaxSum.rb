01d8e043917a938c942d4976263d1818e7348820
Muriel <matheus.muriel@outlook.com>
2019-10-14 14:42:39 -0300

    MiniMaxSum Criado

---
+++ Mini-MaxSum/Mini-MaxSum.rb
@@ -0,0 +1,16 @@
+#!/bin/ruby
+
+#https://www.hackerrank.com/challenges/mini-max-sum/problem
+
+require 'json'
+require 'stringio'
+
+# Complete the miniMaxSum function below.
+def miniMaxSum(arr)
+
+
+end
+
+arr = gets.rstrip.split(' ').map(&:to_i)
+
+miniMaxSum arr
