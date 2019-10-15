4d8d02bbab9328b99d445c9825bee252db589da6
Muriel <matheus.muriel@outlook.com>
2019-10-15 13:29:21 -0300

    gradingStudents correto

--- GrandingStudents/GradingStudents.rb
+++ GrandingStudents/GradingStudents.rb
@@ -11,13 +11,24 @@ require 'stringio'
 # The function is expected to return an INTEGER_ARRAY.
 # The function accepts INTEGER_ARRAY grades as parameter.
 #
+#If the difference between the grade and the next multiple of 5 is less than 3, round grade up to the next multiple of 5.
+#If the value of grade is less than 38, no rounding occurs as the result will still be a failing grade.
 
 def gradingStudents(grades)
-    # Write your code here
+    inf = 1.upto(Float::INFINITY)
+    next_mltp_of_5 = -> (num){inf.lazy.select{|x| x if x % 5 == 0 and x >= num}.first(1)}
+    mults = (0..3).to_a
+    rouder = -> (num){ mults.map{|m| next_mltp_of_5.(num)[0] if (next_mltp_of_5.(num)[0] - (num)) < 3 }.first}
 
+    return grades.map{|grade| 
+        rond = rouder.(grade)
+        if rond.nil? or grade < 38
+            grade
+        else
+            rond
+        end
+    }
 end
-
-fptr = File.open(ENV['OUTPUT_PATH'], 'w')
 
 grades_count = gets.strip.to_i
 
@@ -29,8 +40,3 @@ grades_count.times do |i|
 end
 
 result = gradingStudents grades
-
-fptr.write result.join "\n"
-fptr.write "\n"
-
-fptr.close()
