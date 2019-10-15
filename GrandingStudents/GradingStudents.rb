942c0f32c099e91f66e48d068ae46ff54d1d9a10
Muriel <matheus.muriel@outlook.com>
2019-10-15 12:11:00 -0300

    codigo base GrandingStudents

---
+++ GrandingStudents/GradingStudents.rb
@@ -0,0 +1,36 @@
+#!/bin/ruby
+
+# hackerrank.com/challenges/grading/problem
+
+require 'json'
+require 'stringio'
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades)
+    # Write your code here
+
+end
+
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+grades_count = gets.strip.to_i
+
+grades = Array.new(grades_count)
+
+grades_count.times do |i|
+    grades_item = gets.strip.to_i
+    grades[i] = grades_item
+end
+
+result = gradingStudents grades
+
+fptr.write result.join "\n"
+fptr.write "\n"
+
+fptr.close()
