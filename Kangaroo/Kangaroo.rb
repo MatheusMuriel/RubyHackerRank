2bc2fd73639f6fc2feb8a91cc55034ba71956dd0
Muriel <matheus.muriel@outlook.com>
2019-10-22 18:11:28 -0300

    Kangaroo em limite de tempo

--- Kangaroo/Kangaroo.rb
+++ Kangaroo/Kangaroo.rb
@@ -7,14 +7,20 @@ require 'stringio'
 
 # Complete the kangaroo function below.
 def kangaroo(x1, v1, x2, v2)
-    att_jump = -> (_x1, _x2){[(_x1+v1),(_x2+v2)]}
-    
-    r = 1..Float::INFINITY
-
     if v2 >= v1 
         return 'NO'
     end
-    
+
+    r = 1..Float::INFINITY
+
+    att_jump = -> (_x1, _x2){[(_x1+v1),(_x2+v2)]}
+    anls_rate = -> (jumps) {
+        last_jumps = jumps.reverse_each{|j| j}.first(3)
+        diffs = last_jumps.each.map{|j| j[1] - j[0]}
+        taxa = diffs.each_with_index.map{|dff, i| i > 0 ? dff < diffs[i-1] : true}
+        return taxa.all?
+    }
+
     jumps = [[x1, x2]]
 
     r.lazy.map{
@@ -25,17 +31,11 @@ def kangaroo(x1, v1, x2, v2)
         if nw_ar[0] == nw_ar[1]
             return 'YES'
         end
+        if !anls_rate.(jumps)
+            return 'NO'
+        end
     }.force
-
-    #for i in (1..100)
-    #    ar_ant = jumps[i-1]
-    #    nw_ar = att_jump.(ar_ant[0], ar_ant[1])
-    #    jumps.push(nw_ar)
-    #    if nw_ar[0] == nw_ar[1]
-    #        return 'YES'
-    #    end
-    #end
-
+    
     return 'NO'
 end
 
