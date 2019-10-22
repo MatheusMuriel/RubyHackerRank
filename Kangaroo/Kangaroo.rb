4eeb331b5f37ce079375a5409d487c2fc39f0a67
Muriel <matheus.muriel@outlook.com>
2019-10-22 16:39:14 -0300

    :fire: Criado Kangaroo

---
+++ Kangaroo/Kangaroo.rb
@@ -0,0 +1,31 @@
+#!/bin/ruby
+
+#https://www.hackerrank.com/challenges/kangaroo/problem
+
+require 'json'
+require 'stringio'
+
+# Complete the kangaroo function below.
+def kangaroo(x1, v1, x2, v2)
+
+
+end
+
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+x1V1X2V2 = gets.rstrip.split
+
+x1 = x1V1X2V2[0].to_i
+
+v1 = x1V1X2V2[1].to_i
+
+x2 = x1V1X2V2[2].to_i
+
+v2 = x1V1X2V2[3].to_i
+
+result = kangaroo x1, v1, x2, v2
+
+fptr.write result
+fptr.write "\n"
+
+fptr.close()
