5c228b9dee8c2fe89eddf269357fdafcb1b27085
Muriel <matheus.muriel@outlook.com>
2019-10-23 13:23:03 -0300

    :rocket: Kangaroo resolvido

--- Kangaroo/Kangaroo.rb
+++ Kangaroo/Kangaroo.rb
@@ -11,32 +11,23 @@ def kangaroo(x1, v1, x2, v2)
         return 'NO'
     end
 
-    r = 1..Float::INFINITY
-
     att_jump = -> (_x1, _x2){[(_x1+v1),(_x2+v2)]}
-    anls_rate = -> (jumps) {
-        last_jumps = jumps.reverse_each{|j| j}.first(3)
-        diffs = last_jumps.each.map{|j| j[1] - j[0]}
-        taxa = diffs.each_with_index.map{|dff, i| i > 0 ? dff < diffs[i-1] : true}
-        return taxa.all?
-    }
 
-    jumps = [[x1, x2]]
+    jumps = [x1, x2]
 
-    r.lazy.map{
-        |i|
-        ar_ant = jumps[i-1]
-        nw_ar = att_jump.(ar_ant[0], ar_ant[1])
-        jumps.push(nw_ar)
-        if nw_ar[0] == nw_ar[1]
+    while jumps[0] <= jumps[1] do
+        nw_jump = att_jump.(jumps[0], jumps[1])
+        
+        if nw_jump[0] == nw_jump[1]
             return 'YES'
         end
-        if !anls_rate.(jumps)
-            return 'NO'
-        end
-    }.force
-    
-    return 'NO'
+        
+        jumps = nw_jump
+    end
+
+    if nw_jump[0] != nw_jump[1]
+        return 'NO'
+    end
 end
 
 x1V1X2V2 = gets.rstrip.split
