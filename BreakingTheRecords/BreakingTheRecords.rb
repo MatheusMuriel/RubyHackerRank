4d8c63517aec562c86e7468dc1a43347ebb44b56
Muriel <matheus.muriel@outlook.com>
2019-10-23 14:25:44 -0300

    :fire: Criado Breaking the Records

---
+++ BreakingTheRecords/BreakingTheRecords.rb
@@ -0,0 +1,19 @@
+#!/bin/ruby
+
+#https://www.hackerrank.com/challenges/breaking-best-and-worst-records/
+
+require 'json'
+require 'stringio'
+
+# Complete the breakingRecords function below.
+def breakingRecords(scores)
+    
+end
+
+n = gets.to_i
+
+scores = gets.rstrip.split(' ').map(&:to_i)
+
+result = breakingRecords scores
+
+puts result
