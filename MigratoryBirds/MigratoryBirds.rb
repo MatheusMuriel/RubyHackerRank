f13a73efda0bf70416678aff854e15a45491eb3c
Muriel <matheus.muriel@outlook.com>
2019-10-24 14:16:26 -0300

    Criado Migratory Birds

---
+++ MigratoryBirds/MigratoryBirds.rb
@@ -0,0 +1,20 @@
+#!/bin/ruby
+
+#https://www.hackerrank.com/challenges/migratory-birds
+
+require 'json'
+require 'stringio'
+
+# Complete the migratoryBirds function below.
+def migratoryBirds(arr)
+
+
+end
+
+arr_count = gets.strip.to_i
+
+arr = gets.rstrip.split.map(&:to_i)
+
+result = migratoryBirds arr
+
+puts result
