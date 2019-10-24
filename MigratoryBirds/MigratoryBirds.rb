8472bfd1e7167cb4e30043ca34efb1cbac799692
Muriel <matheus.muriel@outlook.com>
2019-10-24 14:41:54 -0300

    Solucionado o Migratory Birds

--- MigratoryBirds/MigratoryBirds.rb
+++ MigratoryBirds/MigratoryBirds.rb
@@ -7,8 +7,10 @@ require 'stringio'
 
 # Complete the migratoryBirds function below.
 def migratoryBirds(arr)
-
-
+    av_frec = Hash.new(0)
+    frecs = (1..5).each{|av| av_frec[av] = arr.count(av)}
+    max = av_frec.max_by{|k,v| v}[0]
+    return max
 end
 
 arr_count = gets.strip.to_i
