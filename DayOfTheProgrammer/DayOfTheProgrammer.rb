442d69ca6995d2e1b8e5c3f1de562d039d1c8a97
Muriel <matheus.muriel@outlook.com>
2019-10-31 15:33:48 -0300

    Geração do calendario

--- DayOfTheProgrammer/DayOfTheProgrammer.rb
+++ DayOfTheProgrammer/DayOfTheProgrammer.rb
@@ -5,9 +5,12 @@
 require 'json'
 require 'stringio'
 
-# Complete the dayOfProgrammer function below.
+# Complete the dayOfProgrammer function below. 256
 def dayOfProgrammer(year)
 
+    leapYearJulian = -> (year){year % 4 == 0}
+    leapYearGregorian = -> (year){year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)}
+    calendar = (1..12).each.map{|x| (x == 2 ? ((year > 1917 ? (leapYearGregorian.(year) ? 29 : 28) : (leapYearJulian.(year) ? 29 : 28))) : (x > 7 ? x+1 : x) % 2 == 0 ? 30 : 31) }
 
 end
 
