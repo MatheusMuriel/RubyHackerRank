518d739a7d659aa302f0b4b53100bb499b6aad1f
Muriel <matheus.muriel@outlook.com>
2019-10-31 13:45:27 -0300

    :fire: Criado Day of the Programmer

---
+++ DayOfTheProgrammer/DayOfTheProgrammer.rb
@@ -0,0 +1,18 @@
+#!/bin/ruby
+
+#https://www.hackerrank.com/challenges/day-of-the-programmer/problem
+
+require 'json'
+require 'stringio'
+
+# Complete the dayOfProgrammer function below.
+def dayOfProgrammer(year)
+
+
+end
+
+year = gets.strip.to_i
+
+result = dayOfProgrammer year
+
+puts result
