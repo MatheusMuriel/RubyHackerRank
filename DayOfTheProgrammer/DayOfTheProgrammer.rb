361e4bf52a2dbb5d2ec572293fc4592d0f589ab9
Muriel <matheus.muriel@outlook.com>
2019-10-31 16:55:05 -0300

    Correção dos meses

--- DayOfTheProgrammer/DayOfTheProgrammer.rb
+++ DayOfTheProgrammer/DayOfTheProgrammer.rb
@@ -11,8 +11,12 @@ def dayOfProgrammer(year)
     leapYearJulian = -> (year){year % 4 == 0}
     leapYearGregorian = -> (year){year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)}
     calendar = (1..12).each.map{|x| (x == 2 ? ((year > 1917 ? (leapYearGregorian.(year) ? 29 : 28) : (leapYearJulian.(year) ? 29 : 28))) : (x > 7 ? x+1 : x) % 2 == 0 ? 30 : 31) }
-
-end
+    
+    mes = calendar.each_with_index.select{|v, i| i > 0 && calendar[0,i].reduce(:+) >= 256}.first[1] - 1
+    dia = 256 - calendar[0,mes].reduce(:+)
+    
+    return "%02d.%02d.%02d" % [dia, mes+1, year]
+end 
 
 year = gets.strip.to_i
 
