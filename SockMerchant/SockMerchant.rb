cfa6d1b766e59a431302ad69add2a36ef738553a
Matheus Muriel <matheus.muriel@outlook.com>
2019-11-06 19:04:04 -0300

    :rocket: Solucionado Sock Merchant

--- SockMerchant/SockMerchant.rb
+++ SockMerchant/SockMerchant.rb
@@ -7,8 +7,9 @@ require 'stringio'
 
 # Complete the sockMerchant function below.
 def sockMerchant(n, ar)
-
-
+  hs_socks = ar.group_by{|x| x}
+  pairs_count = hs_socks.map{|x| x[1].length.even? ? (x[1].length / 2) : ((x[1].length-1) / 2)}
+  return pairs_count.reduce(:+)
 end
 
 n = gets.to_i
