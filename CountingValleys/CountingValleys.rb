26fab9fdc22e292e9562e8f70e891d2730290e6b
Muriel <matheus.muriel@outlook.com>
2019-11-27 17:50:27 -0300

    :fire: Criado Counting Valleys

---
+++ CountingValleys/CountingValleys.rb
@@ -0,0 +1,20 @@
+#!/bin/ruby
+
+# https://www.hackerrank.com/challenges/counting-valleys/problem
+
+require 'json'
+require 'stringio'
+
+# Complete the countingValleys function below.
+def countingValleys(n, s)
+
+
+end
+
+n = gets.to_i
+
+s = gets.to_s.rstrip
+
+result = countingValleys n, s
+
+puts result
