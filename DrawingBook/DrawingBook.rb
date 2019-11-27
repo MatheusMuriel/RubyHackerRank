a1e779c4ea7d031e8f4d68451c96d757139983bb
Muriel <matheus.muriel@outlook.com>
2019-11-27 16:52:36 -0300

    :fire: Criado Drawing Book

---
+++ DrawingBook/DrawingBook.rb
@@ -0,0 +1,21 @@
+#!/bin/ruby
+
+#https://www.hackerrank.com/challenges/drawing-book/problem
+
+#
+# Complete the pageCount function below.
+#
+def pageCount(n, p)
+    #
+    # Write your code here.
+    #
+
+end
+
+n = gets.to_i
+
+p = gets.to_i
+
+result = pageCount n, p
+
+puts result
